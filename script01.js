51a65987e352df5d39e92179048fe3eb77e34d0c
ybmahadali <44368646+ybmahadali@users.noreply.github.com>
2018-10-25 16:27:18 +0200

    Update script01.js

--- script01.js
+++ script01.js
@@ -1,6 +1,6 @@
 function createNode(element)
 {
-return document.createElement(element);
+	return document.createElement(element);
 }
 function append(parent,el)
 {
@@ -9,12 +9,10 @@ function append(parent,el)
 
 const ul= document.getElementById("authors");
 
-
-
 fetch("https://acobot-brainshop-ai-v1.p.mashape.com/get?bid=178&key=sX5A2PcYZbsN5EY6&uid=mashape&msg=what%20do%20you%20like", {
   headers: {
-    Accept: "application/json",
-    "X-Mashape-Key": "XTAJ1AQfbzmshw3qVeLKUuRLLEUap19S13hjsnn3Dv62dSd9VA"
+    	Accept: "application/json",
+   	"X-Mashape-Key": "XTAJ1AQfbzmshw3qVeLKUuRLLEUap19S13hjsnn3Dv62dSd9VA"
   }
 })
 .then((resp)=>resp.json())
@@ -26,8 +24,7 @@ fetch("https://acobot-brainshop-ai-v1.p.mashape.com/get?bid=178&key=sX5A2PcYZbsN
 	span.innerHTML = authors.cnt;
 	append(li,span);
 	append(ul,li);
-	
-	
+		
 	})
 .catch(function(error){
 console.log(JSON.stringify(error));
