c6ffc24ed9297cb0ea1a7cd5743c58f6ca744cdb
root <root@DESKTOP-5RMGQ7L.localdomain>
2018-10-25 16:20:52 +0200

    ajout script Sentiment Analysis.js

---
+++ scriptSentAnalysis.js
@@ -0,0 +1,49 @@
+function createNode(element)
+{
+return document.createElement(element);
+}
+function append(parent,el)
+{
+	return parent.appendChild(el);
+}
+
+const ul= document.getElementById("authors");
+
+const url='https://randomuser.me/api/?results=10';
+
+fetch("https://text-sentiment.p.mashape.com/analyze", {
+  body: "text=I am so sad",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/x-www-form-urlencoded",
+    "X-Mashape-Key": "XTAJ1AQfbzmshw3qVeLKUuRLLEUap19S13hjsnn3Dv62dSd9VA"
+  },
+  method: "POST"
+})
+.then((resp)=>resp.json())
+.then(function(data){
+	let authors = data;
+	let li1= createNode('li'),
+	li2= createNode('li'),
+	li3= createNode('li'), 
+	span1 = createNode('h2'); 
+	span2 = createNode('h2'); 
+	span3 = createNode('h2');
+	var pos= "positive emotion ";
+	var neg= "negative emotion ";
+	var mid= "i don't know ";
+	span1.innerHTML = pos.concat(authors.pos_percent);
+	span2.innerHTML = neg.concat(authors.neg_percent);
+	span3.innerHTML = mid.concat(authors.mid_percent);
+	append(li1,span1);
+	append(li2,span2);
+	append(li3,span3);
+	append(ul,li1);
+	append(ul,li2);
+	append(ul,li3);
+	
+	
+	})
+.catch(function(error){
+console.log(JSON.stringify(error));
+});
