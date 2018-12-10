7427ea16e9496308ca6c2a4c10f912c2be52eaa1
MELGABSI <moezelgabsi@hotmail.fr>
2018-12-10 21:29:26 +0100

    add specifications to script.js

--- script.js
+++ script.js
@@ -7,9 +7,7 @@ function append(parent,el)
 	return parent.appendChild(el);
 }
 
-const ul= document.getElementById("authors");
-
-const url='https://randomuser.me/api/?results=10';
+const ul= document.getElementById("sentiments");
 
 fetch("https://text-sentiment.p.mashape.com/analyze", {
   body: "text=I am so sad",
@@ -22,7 +20,7 @@ fetch("https://text-sentiment.p.mashape.com/analyze", {
 })
 .then((resp)=>resp.json())
 .then(function(data){
-	let authors = data;
+	let sentiments = data;
 	let li1= createNode('li'),
 	li2= createNode('li'),
 	li3= createNode('li'), 
@@ -32,9 +30,9 @@ fetch("https://text-sentiment.p.mashape.com/analyze", {
 	var pos= "positive emotion ";
 	var neg= "negative emotion ";
 	var mid= "i don't know ";
-	span1.innerHTML = pos.concat(authors.pos_percent);
-	span2.innerHTML = neg.concat(authors.neg_percent);
-	span3.innerHTML = mid.concat(authors.mid_percent);
+	span1.innerHTML = pos.concat(sentiments.pos_percent);
+	span2.innerHTML = neg.concat(sentiments.neg_percent);
+	span3.innerHTML = mid.concat(sentiments.mid_percent);
 	append(li1,span1);
 	append(li2,span2);
 	append(li3,span3);
