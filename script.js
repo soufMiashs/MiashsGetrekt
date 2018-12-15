aeef390334ad82166ea627981dd3cba60ffad8b0
MELGABSI <moezelgabsi@hotmail.fr>
2018-12-15 15:47:05 +0100

    Rename script

--- script.js
+++ script.js
@@ -65,4 +65,28 @@ fetch("https://text-sentiment.p.mashape.com/analyze", {
 .catch(function(error){
 console.log(JSON.stringify(error));
 });
+	          
+	
+	lastUserMessage = document.getElementById("myText").value;
+           lastUserMessage = lastUserMessage.replace(/ +/g, "%20");
+           fetch("https://acobot-brainshop-ai-v1.p.mashape.com/get?bid=178&key=sX5A2PcYZbsN5EY6&uid=mashape&msg="+lastUserMessage, {
+            headers: {
+              Accept: "application/json",
+             "X-Mashape-Key": "XTAJ1AQfbzmshw3qVeLKUuRLLEUap19S13hjsnn3Dv62dSd9VA"
+                 }
+                })
+                .then((resp)=>resp.json())
+                .then(function(data){
+                    let reponse = data;
+                    
+                    let li= createNode('li'),
+                    span = createNode('h2');
+                    span.innerHTML = reponse.cnt;
+                    append(li,span);
+                    append(ul,li);
+                        
+                    })
+                .catch(function(error){
+                console.log(JSON.stringify(error));
+                });
 }
